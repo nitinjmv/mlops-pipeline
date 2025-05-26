48c39783a9512946259932c5b743dc36ef85dfdd
nitinjmv <nitinjmv@gmail.com>
2025-05-26 22:05:08 +0100

    test

--- src/data_pipeline/data_preprocessing.py
+++ src/data_pipeline/data_preprocessing.py
@@ -9,29 +9,10 @@ import nltk
 nltk.download('stopwords')
 nltk.download('punkt_tab')
 
-from utils.mlflow import dagshub_integration
+from src.utils.commons import load_params, logging_setup
+from src.utils.mlflow import dagshub_integration
 
-# Ensure the "logs" directory exists
-log_dir = 'logs'
-os.makedirs(log_dir, exist_ok=True)
-
-# Setting up logger
-logger = logging.getLogger('data_preprocessing')
-logger.setLevel('DEBUG')
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel('DEBUG')
-
-log_file_path = os.path.join(log_dir, 'data_preprocessing.log')
-file_handler = logging.FileHandler(log_file_path)
-file_handler.setLevel('DEBUG')
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(formatter)
-file_handler.setFormatter(formatter)
-
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+logger = logging_setup('data_preprocessing')
 
 def transform_text(text):
     """
