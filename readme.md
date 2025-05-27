b6bd2e127941d4e0e7e06dc708b4a5c67a5dbbae
nitinjmv <nitinjmv@gmail.com>
2025-05-27 22:13:57 +0100

    test

---
+++ readme.md
@@ -0,0 +1,15 @@
+Create virtual env
+```bash
+python -m venv .venv
+```
+
+Activate venv
+```bash
+.\.venv\Scripts\Activate.ps1
+```
+
+Install libraries
+```bash
+pip install -r requirements.txt & pip upgrade
+```
+
---
+++ src/utils/mlflow.py
@@ -0,0 +1,22 @@
+from src.utils.commons import load_params, logging_setup
+import mlflow
+import dagshub
+
+params = load_params(params_path='./params.yaml')
+experiment_name = params['mlflow']['experiment_name']
+repo_owner = params['mlflow']['repo_owner']
+repo_name = params['mlflow']['repo_name']
+tracking_uri = params['mlflow']['tracking_uri']
+
+logger = logging_setup('mlflow')
+logger.debug('experiment_name %s', experiment_name)
+logger.debug('repo_owner %s', repo_owner)
+logger.debug('repo_name %s', repo_name)
+logger.debug('tracking_uri %s', tracking_uri)
+
+def dagshub_integration():
+    dagshub.init(repo_owner = repo_owner, repo_name = repo_name, mlflow=True)
+    mlflow.set_tracking_uri(tracking_uri)
+    mlflow.autolog()
+    mlflow.set_experiment(experiment_name)
+    return dagshub
