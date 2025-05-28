6072b493c05e344166c4a0e1f7b6fda6882042f8
nitinjmv <nitinjmv@gmail.com>
2025-05-28 16:53:40 +0100

    test

--- src/utils/fetch_model.py
+++ src/utils/fetch_model.py
@@ -3,7 +3,7 @@ import mlflow
 mlflow.set_tracking_uri("https://dagshub.com/nitinjmv/mlops-pipeline.mlflow")
 
 # Fetch experiment by name
-experiment = mlflow.get_experiment_by_name("your-experiment-name")
+experiment = mlflow.get_experiment_by_name("model_evaluation_experiment")
 if experiment is None:
     print("Experiment not found")
 else:
@@ -11,3 +11,7 @@ else:
     
     print("Available columns:", runs.columns.tolist())  # Check what exists
     print(runs[['run_id', 'status', 'start_time']])  # Print safely
+
+
+# best_run = runs.sort_values(by='metrics.accuracy', ascending=False).iloc[0]
+# print("Best run ID:", best_run['run_id'])
