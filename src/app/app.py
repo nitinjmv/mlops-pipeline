d266435ca821c1389e9864aadb47b929c315511e
nitinjmv <nitinjmv@gmail.com>
2025-05-30 16:19:40 +0100

    gcp setup

--- src/app/app.py
+++ src/app/app.py
@@ -40,5 +40,5 @@ def index():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
